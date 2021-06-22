42781ea800311a29db5027aa2d56238f02644afa
jdavidrcamacho <jdavidrcamacho@gmail.com>
2021-06-22 17:14:40 +0100

    mooooore

--- sunData/01_periodograms.py
+++ sunData/01_periodograms.py
@@ -59,13 +59,22 @@ axs[3,0].tick_params(axis='both', which='both', labelbottom=True)
 from astropy.timeseries import LombScargle
 linesize = 1
 
+
+per = 196.560
+
+
 f1, p1 = LombScargle(time, rv, rverr).autopower()
 axs[0,1].semilogx(1/f1, p1, color='blue', linewidth=linesize)
 bestf = f1[np.argmax(p1)]
 bestp = 1/bestf
 axs[0,1].axvline(x=27, ymin=p1.min(), ymax=100*p1.max(), color='red', alpha=1,
                linewidth=linesize)
+axs[0,1].axvline(x=per, ymin=p1.min(), ymax=100*p1.max(), color='orange', alpha=1,
+               linewidth=linesize)
+axs[0,1].axvline(x=365.256/2.0, ymin=p1.min(), ymax=100*p1.max(), color='cyan', alpha=1,
+               linewidth=linesize)
 axs[0,1].tick_params(axis='both', which='both', labelbottom=False)
+
 #false alarm
 falseAlarms1 = LombScargle(time, rv, rverr).false_alarm_level([0.1,0.01,0.001])
 one = falseAlarms1[1] * np.ones_like(f1)
@@ -79,6 +88,8 @@ axs[1,1].set_ylabel('Normalized power')
 bestf = f2[np.argmax(p2)]
 bestp = 1/bestf
 axs[1,1].axvline(x=27, ymin=p2.min(), ymax=100*p2.max(), color='red', alpha=1,
+               linewidth=linesize)
+axs[1,1].axvline(x=per, ymin=p1.min(), ymax=100*p1.max(), color='orange', alpha=1,
                linewidth=linesize)
 axs[1,1].tick_params(axis='both', which='both', labelbottom=False)
 #false alarm
@@ -94,6 +105,8 @@ bestf = f3[np.argmax(p3)]
 bestp = 1/bestf
 axs[2,1].axvline(x=27, ymin=p3.min(), ymax=100*p3.max(), color='r', alpha=1,
                linewidth=linesize)
+axs[2,1].axvline(x=per, ymin=p1.min(), ymax=100*p1.max(), color='orange', alpha=1,
+               linewidth=linesize)
 axs[2,1].tick_params(axis='both', which='both', labelbottom=False)
 falseAlarms3 = LombScargle(time, fw, fwerr).false_alarm_level([0.1,0.01,0.001])
 one = falseAlarms3[1] * np.ones_like(f3)
@@ -107,6 +120,8 @@ bestf = f4[np.argmax(p4)]
 bestp = 1/bestf
 axs[3,1].axvline(x=27, ymin=p4.min(), ymax=100*p4.max(), color='red', alpha=1,
                linewidth=linesize)
+axs[3,1].axvline(x=per, ymin=p1.min(), ymax=100*p1.max(), color='orange', alpha=1,
+               linewidth=linesize)
 axs[3,1].tick_params(axis='both', which='both', labelbottom=True)
 axs[3,1].set_xlabel('Period (days)')
 #false alarm
@@ -116,7 +131,7 @@ axs[3,1].plot(1/f4, one, color='red', linestyle='dashed', alpha=1, linewidth=lin
 axs[3,1].set_xlim(1,time.ptp())
 
 plt.tight_layout(h_pad=0.7, w_pad=0.7)
-plt.savefig('01_periodograms.pdf', bbox_inches='tight')
+plt.savefig('01_periodograms_b.pdf', bbox_inches='tight')
 plt.close('all')
 
 
